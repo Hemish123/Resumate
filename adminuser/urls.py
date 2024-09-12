5c536e2d4009d5bf30ba2e5fcf3578e7f9b6a3f8
Nikita Kapadiya <nikikapadiya97@gmail.com>
2024-09-12 17:46:34 +0530

    updated

--- adminuser/urls.py
+++ adminuser/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path
-from .views import CreateEmployeeView, EmployeeUpdateView
+from .views import CreateEmployeeView, UserUpdateView
 #
 urlpatterns = [
     path('create-employee/', CreateEmployeeView.as_view(), name='create-employee'),
-    path('update-employee/<int:pk>/', EmployeeUpdateView.as_view(), name='update-employee'),
+    path('update-employee/<int:pk>/', UserUpdateView.as_view(), name='update-employee'),
 
 ]
