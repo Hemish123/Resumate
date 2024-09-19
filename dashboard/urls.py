c10213e48b2f54c41a101f6538ec6933f0f2599f
Nikita Kapadiya <nikikapadiya97@gmail.com>
2024-09-19 18:24:35 +0530

    updated

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
-from .views import DashbaordView, StageView,StageAPIView, CandidateAPIView
+from .views import DashbaordView, StageView,StageAPIView, CandidateAPIView, CalendarView
 
 
 urlpatterns = [
     path('', DashbaordView.as_view(), name='dashboard'),
     path('job-process/<int:pk>/', StageView.as_view(), name='job-process'),
     path('stage-api/<int:pk>/', StageAPIView.as_view(), name='stage-api'),
-    path('candidate-api/', CandidateAPIView.as_view(), name='candidate-api')
+    path('candidate-api/', CandidateAPIView.as_view(), name='candidate-api'),
+    path('calendar/', CalendarView.as_view(), name='calendar')
+
 ]
 
 
