1ed0a556a5665c375c0184ac0df7020fbff8da8e
Hemish123 <consultant@jmsadvisory.in>
2025-12-02 10:12:02 +0530

    Update settings.py

--- recruit_management/settings.py
+++ recruit_management/settings.py
@@ -233,13 +233,13 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = "9ca506001@smtp-brevo.com"
+EMAIL_HOST_PASSWORD = "xsmtpsib-e1cf685ded2e9aa497c450a6d715ebdeb2185bcbf82db2bb11bea349fd0da094-1i6mvzzCgJX0Vso9"
 
-DEFAULT_FROM_EMAIL = f"Recruit Smart <{os.environ['EMAIL_HOST_USER']}>"
+DEFAULT_FROM_EMAIL = f"Recruit Smart <recruitsmart@hirewithjms.com>"
 
 # link expire after 10 minutes
 EXPIRE_AFTER = "10m"
