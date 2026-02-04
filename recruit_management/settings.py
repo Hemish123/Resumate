5a23318b3966dd3ab6150183d6bbd87a3e8968f5
Hemish123 <consultant@jmsadvisory.in>
2026-02-04 12:02:16 +0530

    Update settings.py

--- recruit_management/settings.py
+++ recruit_management/settings.py
@@ -238,7 +238,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = "resumate1nfo1@gmail.com"
 # EMAIL_HOST_PASSWORD = 'uzuc bhya nbav wmhc'
-MAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 DEFAULT_FROM_EMAIL = f"Recruit Smart <resumate1nfo1@gmail.com>"
